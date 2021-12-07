ea640d2f715d0e373c2c923e39f6820973ce5ec1
Kadekk01 <dekyudiarsana26@gmail.com>
2021-12-07 09:17:55 +0800

    connect ke database

---
+++ config/config.php
@@ -0,0 +1,33 @@
+<?php
+// setting default timezone 
+date_default_timezone_set('asia/hong_kong');
+
+// session start
+session_start();
+
+// koneksi ke database
+$con=mysqli_connect('localhost','root','','presensi');
+if(mysqli_connect_errno()){
+    echo mysqli_connect_error();
+}
+
+// fungsi base_url
+function base_url($url = null){
+    $base_url = "http://localhost/Klp3_ProjectWeb";
+    if($url != null){
+        return $base_url."/".$url;
+    }
+    else{
+        return $base_url;
+    }
+}
+
+// function untuk tgl format indonesia
+function tgl_indo($tgl){
+	$tanggal = substr($tgl, 8, 2);
+	$bulan = substr($tgl, 5, 2);
+	$tahun = substr($tgl, 0, 4);
+
+	return $tanggal."/".$bulan."/".$tahun;
+}
+?>
